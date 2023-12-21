26fe38e2dffc6c87fbba746a41e37749a3624627
doneML <doneML@qq.com>
2023-12-21 09:54:55 +0800

    初始化springboot项目

---
+++ src/main/java/com/ml/MybatisMlApplication.java
@@ -0,0 +1,13 @@
+package com.ml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisMlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisMlApplication.class, args);
+    }
+
+}
